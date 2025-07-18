d43a0b06baa968930e50eaca85c336c53730577f
Anurag19-web <anuragnayak78910@gmail.com>
2025-07-18 12:02:09 +0530

    database changes added

--- config/dataBase.js
+++ config/dataBase.js
@@ -1,10 +1,14 @@
-// db.js
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const connectDB = async () => {
   try {
-    // await mongoose.connect("mongodb://localhost:27017/Dev-Platform-DB");
-     await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      // These are no longer needed, but harmless
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("✅ MongoDB connected");
   } catch (err) {
     console.error("❌ MongoDB connection error:", err.message);
