e28f1b734c8ba12e98c8bfccb342c8b4d8b151a1
Anurag19-web <anuragnayak78910@gmail.com>
2025-07-18 11:26:20 +0530

    changes in database file

--- config/dataBase.js
+++ config/dataBase.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/Dev-Platform-DB");
+    // await mongoose.connect("mongodb://localhost:27017/Dev-Platform-DB");
+     await mongoose.connect(process.env.MONGO_URI);
     console.log("✅ MongoDB connected");
   } catch (err) {
     console.error("❌ MongoDB connection error:", err.message);
