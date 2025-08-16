a7f3e15ffc8fe4c3cc3d4cb66176fde27e245d14
Anurag19-web <anuragnayak78910@gmail.com>
2025-08-16 13:13:11 +0530

    changes for solve Invalid Signature error

--- config/cloudinaryConfig.js
+++ config/cloudinaryConfig.js
@@ -1,12 +1,11 @@
 // config/cloudinaryConfig.js
 import { v2 as cloudinary } from "cloudinary";
-import dotenv from "dotenv";
 
-dotenv.config();
-
-// Auto-detect from CLOUDINARY_URL
 cloudinary.config({
-  secure: true, // optional, forces https
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true, // optional
 });
 
 console.log("Cloudinary config:", cloudinary.config());
