fc9ed0de75535f3aa7c9f4f99f8b3b58ebd2eb61
Anurag19-web <anuragnayak78910@gmail.com>
2025-08-16 12:45:17 +0530

    changes for render env

--- config/cloudinaryConfig.js
+++ config/cloudinaryConfig.js
@@ -1,13 +1,14 @@
+// config/cloudinaryConfig.js
 import { v2 as cloudinary } from "cloudinary";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+// Auto-detect from CLOUDINARY_URL
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, // from your Cloudinary dashboard
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
+  secure: true, // optional, forces https
 });
 
-console.log("Cloudinary Key:", process.env.CLOUDINARY_API_KEY);
-console.log("Cloudinary Secret:", process.env.CLOUDINARY_API_SECRET);
-
+console.log("Cloudinary config:", cloudinary.config());
 
 export default cloudinary;
