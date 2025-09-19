d45efa0c2624e8a063d016bff70b245b78e114b7
Anurag19-web <anuragnayak78910@gmail.com>
2025-09-19 16:59:11 +0530

    changes for dynamically update comments data

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 // models/User.js
 import mongoose from "mongoose";
+import { Post } from "./Post";
 
 const userSchema = new mongoose.Schema({
   userId: { type: String, required: true, unique: true },
@@ -28,6 +29,29 @@ const userSchema = new mongoose.Schema({
     }
   ]
 }, { timestamps: true });
+
+/* ------------------ Middleware to update comments on username/profilePicture change ------------------ */
+userSchema.pre('save', async function(next) {
+  try {
+    // Check if username or profilePicture is modified
+    if (this.isModified('username') || this.isModified('profilePicture')) {
+      await Post.updateMany(
+        { "comments.userId": this.userId },
+        { 
+          $set: { 
+            "comments.$[elem].username": this.username, 
+            "comments.$[elem].profilePicture": this.profilePicture 
+          } 
+        },
+        { arrayFilters: [{ "elem.userId": this.userId }] }
+      );
+    }
+    next();
+  } catch (err) {
+    console.error("Error updating comments on user update:", err);
+    next(err);
+  }
+});
 
 const User = mongoose.model("User", userSchema);
 export default User;
